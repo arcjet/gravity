a17e1fc7c9dabdd254758e5c0de099accd95ec97
Blaine <blaine@arcjet.com>
2026-01-05 11:45:30 -0700

    enums and bools as uint64

--- cmd/gravity/src/codegen/imported_func.rs
+++ cmd/gravity/src/codegen/imported_func.rs
@@ -144,7 +144,7 @@ impl Bindgen for ImportedFunc<'_> {
                 let operand = &operands[0];
                 quote_in! { self.body =>
                     $['\r']
-                    var $(&value) uint32
+                    var $(&value) uint64
                     if $operand {
                         $(&value) = 1
                     } else {
@@ -777,7 +777,7 @@ impl Bindgen for ImportedFunc<'_> {
 
                 quote_in! { self.body =>
                     $['\r']
-                    var $enum_tmp uint32
+                    var $enum_tmp uint64
                     switch $value {
                     $cases
                     default:
